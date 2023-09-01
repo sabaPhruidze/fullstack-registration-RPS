e17bb64a010626b7f2b8bcc3336bf4d1d90315a6
Saba Phruidze <sabaphruidze01@gmail.com>
2023-09-01 15:29:46 +0400

    I will try to build logic of rock-paper-scissors first and then I will correct the visual part

--- Frontend/src/components/Game.tsx
+++ Frontend/src/components/Game.tsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import paper from "../assets/img/paper.svg";
+import scissors from "../assets/img/scissors.svg";
+import rock from "../assets/img/rock.svg";
+import press from "../assets/img/button.svg";
+import "./pieces/styled.css";
 
 export default function Game() {
   return (
-    <div>Game</div>
-  )
+    <div>
+      <div className="wrapper">
+        <div className="buttons">
+          <button className="rpsButton" value="Rock">
+            ✊
+          </button>
+          <button className="rpsButton" value="Paper">
+            🤚
+          </button>
+          <button className="rpsButton" value="Scissors">
+            ✌️
+          </button>
+        </div>
+        <div className="resultContainer">
+          <div id="player-score"></div>
+          <div id="hands"></div>
+          <div id="result"></div>
+          <button id="endGameButton">🔴</button>
+        </div>
+      </div>
+    </div>
+  );
 }
